80925b1d357ccbc8bad14ef648bcae9f85802706
Tommy Vo Tran <tomvotran@ucla.edu>
2020-05-14 00:38:33 -0700

    Added preliminary Card. Assembling cards into rows and expanding cards into modals is to be implemented.

--- exchange/src/App.js
+++ exchange/src/App.js
@@ -2,7 +2,20 @@ import React from 'react';
 import './App.css';
 import { Container } from './components/Container';
 import { Filler } from './components/Filler';
+import { Card } from './components/Card'
 
+let examplePost = {
+	typeOfPost: 0, //0,1 = offer, request
+	typeOfItem: 0, //0,1 = textbook, notes		
+	course: 'CS33',
+	itemName: 'Computer Organization: A Perspective!',
+	condition: 0, //from 0 (best) to 3 (worst)
+	description: 'Awesome. I dropped this off at your house when I went to the',
+	link: '',
+	fulfilled: 0, //Unfulfilled, Pending (matched w at least 1 other person), Fulfilled (marked for deletion). On the backend, we will switch pending to fulfilled after 2 weeks have passed.
+	publication_date: new Date(2023, 4, 13),
+	author: 'Tommy', 		// the author’s username
+}
 const App = () => {
 	const triggerText = 'Offer/Request';
 	const onSubmit = (event) => {
@@ -16,6 +29,7 @@ const App = () => {
 		<div className="App">
 			<Filler />
 			<Container triggerText={triggerText} onSubmit={onSubmit} />
+			<Card post={examplePost} />
 			<Filler />
 		</div>
 	);
---
+++ exchange/src/components/Card.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import './Card.css';
+
+export class Card extends React.Component {
+	constructor(props) {
+		super(props);
+	}
+	
+	render() { //TODO: change offer/req indicator to be color of card
+		return (<div>
+			<div className="row">
+				<div className="card w-25 bg-light mb-3">
+					<div className="card-header">
+						<div> {this.props.post.course} </div>
+						<span class="badge badge-primary float-left">Offer</span>
+						<span> {this.props.post.publication_date.toISOString().slice(0, 10)} </span>
+					</div>
+
+					<div className="card-body">
+						<h5 className="card-title">{this.props.post.itemName}</h5>
+						<p className="card-text">{this.props.post.description}</p>
+						<span class="badge  float-left">By {this.props.post.author}</span>
+						<span class="badge badge-purple float-right">100 RP</span>
+
+					</div>
+				</div>
+
+			</div>
+		</div>);
+
+	}
+	
+}
+export default Card;
+//Use this method - it does handle double digits correctly
+Date.prototype.yyyymmdd = function () {
+	var mm = (this.getMonth() + 1).toString(); // getMonth() is zero-based
+	var dd = this.getDate().toString();
+
+	return [this.getFullYear(), mm.length === 2 ? '' : '0', mm, dd.length === 2 ? '' : '0', dd].join(''); // padding
+};
