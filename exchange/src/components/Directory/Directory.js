91a5e65ef57e7a2334c7fc647e9e97420e7deb27
Tony Xia <summertony717@gmail.com>
2020-05-20 14:09:32 -0700

    directory can now make requests to the server properly

--- exchange/src/components/Directory/Directory.js
+++ exchange/src/components/Directory/Directory.js
@@ -7,7 +7,7 @@ import './directory.css';
 function Directory(props) {
     const [queryObject, setQueryObject] = useState({});
     const [firstVisit, setFirstVisit] = useState(true);
-    const welcomeHeader = useRef(null);
+    const [results, setResults] = useState([]);
 
     const handleSearch = (value) => {
         // get query results
@@ -18,7 +18,23 @@ function Directory(props) {
         if (firstVisit) {
             setFirstVisit(false);
         }
-        
+
+        // fetch the query
+        try {
+            let url = new URL("http://localhost:8000/post/search");
+            let params = value;
+            Object.keys(params).forEach(key => url.searchParams.append(key, params[key]));
+            console.log(url);
+            fetch(url, {
+                method: "GET",
+                headers: {"Content-Type": "application/json"},
+            })
+            .then(a => a.json())
+            .then(b => console.log(b));
+        } catch (e) {
+            console.log(e);
+        }
+
     }
 
     return (
