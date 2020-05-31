52aac550acc5eae46675ee3b7f3659c6b0bf7394
Tommy Vo Tran <tomvotran@ucla.edu>
2020-05-31 01:36:33 -0700

    Temp commented out ClientsDropdown

--- exchange/src/components/Profile/ClientsDropdown.js
+++ exchange/src/components/Profile/ClientsDropdown.js
@@ -1,79 +1,79 @@
-import React from 'react'
-import '../Card.css'
-import { DropdownMultiple, Dropdown } from 'reactjs-dropdown-component'
-import './ClientsDropdown.css';
+// import React from 'react'
+// import '../Card.css'
+// import { DropdownMultiple, Dropdown } from 'reactjs-dropdown-component'
+// import './ClientsDropdown.css';
 
-export class ClientsDropdown extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = { //A dictionary of keys to JSON objects, each of which is an Array of JSON obj entries
-			// clients: [
-			// 	{
-			// 		id: 0,
-			// 		title: 'Bob',
-			// 		selected: false,
-			// 		key: 'clients', //FIRST FOUR ARE MANDATORY FOR LIRBARY TO WORK
-			// 		rp: 10
+// export class ClientsDropdown extends React.Component {
+// 	constructor(props) {
+// 		super(props);
+// 		this.state = { //A dictionary of keys to JSON objects, each of which is an Array of JSON obj entries
+// 			// clients: [
+// 			// 	{
+// 			// 		id: 0,
+// 			// 		title: 'Bob',
+// 			// 		selected: false,
+// 			// 		key: 'clients', //FIRST FOUR ARE MANDATORY FOR LIRBARY TO WORK
+// 			// 		rp: 10
 
-			// 	},
-			// 	{npm l
-			// 		id: 1,
-			// 		title: 'Jennifer',
-			// 		selected: false,
-			// 		key: 'clients',
-			// 		email: 'Bob@cs.ucla.edu',
-			// 		rp: 15
-			// 	},
-			// ]
-			clients: this.parseClients()
-		}
-	}
+// 			// 	},
+// 			// 	{npm l
+// 			// 		id: 1,
+// 			// 		title: 'Jennifer',
+// 			// 		selected: false,
+// 			// 		key: 'clients',
+// 			// 		email: 'Bob@cs.ucla.edu',
+// 			// 		rp: 15
+// 			// 	},
+// 			// ]
+// 			clients: this.parseClients()
+// 		}
+// 	}
 
-	parseClients() {
-		const myClients = []
-		for(let i = 0; this.props.clients && i < this.props.clients.length; i++) {
-			myClients.push(
-				{
-					id: i, //this.props.clients[i]._id,
-					title: this.props.clients[i]['username'] + '  (' + this.props.clients[i]['rp'] + ' RP)',
-					selected: false,
-					key: 'clients',
-					rp: this.props.clients[i]['rp'],
-					databaseID: this.props.clients[i]._id
-				}
-			)
-		}
-		return myClients;
-	}
+// 	parseClients() {
+// 		const myClients = []
+// 		for(let i = 0; this.props.clients && i < this.props.clients.length; i++) {
+// 			myClients.push(
+// 				{
+// 					id: i, //this.props.clients[i]._id,
+// 					title: this.props.clients[i]['username'] + '  (' + this.props.clients[i]['rp'] + ' RP)',
+// 					selected: false,
+// 					key: 'clients',
+// 					rp: this.props.clients[i]['rp'],
+// 					databaseID: this.props.clients[i]._id
+// 				}
+// 			)
+// 		}
+// 		return myClients;
+// 	}
 
-	resetThenSet = (id, key) => {
-		let temp = JSON.parse(JSON.stringify(this.state[key]));
-		temp.forEach(item => item.selected = false);
-		temp[id].selected = true;
-		this.props.selectClient(temp[id].databaseID);
-		this.setState({
-			[key]: temp
-		});
-	}
-	//For multiple selection only. Currently not used.
-	toggleItem = (id, key) => {
-		let temp = JSON.parse(JSON.stringify(this.state[key]));
-		temp[id].selected = !temp[id].selected;
-		this.setState({
-			[key]: temp
-		});
-	}
+// 	resetThenSet = (id, key) => {
+// 		let temp = JSON.parse(JSON.stringify(this.state[key]));
+// 		temp.forEach(item => item.selected = false);
+// 		temp[id].selected = true;
+// 		this.props.selectClient(temp[id].databaseID);
+// 		this.setState({
+// 			[key]: temp
+// 		});
+// 	}
+// 	//For multiple selection only. Currently not used.
+// 	toggleItem = (id, key) => {
+// 		let temp = JSON.parse(JSON.stringify(this.state[key]));
+// 		temp[id].selected = !temp[id].selected;
+// 		this.setState({
+// 			[key]: temp
+// 		});
+// 	}
 
-	render() {
-		return(
-			<Dropdown
-				title="Accept a client"
-				searchable={["Search for client", "No matching client"]}
-				list={this.state.clients}
-				resetThenSet={this.resetThenSet}
-			/>
-		);
-	}
+// 	render() {
+// 		return(
+// 			<Dropdown
+// 				title="Accept a client"
+// 				searchable={["Search for client", "No matching client"]}
+// 				list={this.state.clients}
+// 				resetThenSet={this.resetThenSet}
+// 			/>
+// 		);
+// 	}
 
-}
-export default ClientsDropdown
+// }
+// export default ClientsDropdown
