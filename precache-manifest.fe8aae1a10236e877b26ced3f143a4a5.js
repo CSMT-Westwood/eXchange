00333e11babeeaff4d39f38d17ecd9fbfa989127
Tony Xia <summertony717@gmail.com>
2020-06-15 15:16:49 -0700

    Updates

---
+++ precache-manifest.fe8aae1a10236e877b26ced3f143a4a5.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df7f332410e8784a3f0778b317950fa3",
+    "url": "/eXchange/index.html"
+  },
+  {
+    "revision": "ebd6aef5daad2a8c52c9",
+    "url": "/eXchange/static/css/main.9a017c69.chunk.css"
+  },
+  {
+    "revision": "ee63336e66d6ed5b9010",
+    "url": "/eXchange/static/js/2.9c4141e5.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/eXchange/static/js/2.9c4141e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ebd6aef5daad2a8c52c9",
+    "url": "/eXchange/static/js/main.6254a38f.chunk.js"
+  },
+  {
+    "revision": "720f61212e63954d38b9",
+    "url": "/eXchange/static/js/runtime-main.1560fd82.js"
+  },
+  {
+    "revision": "bf58f165732c7ab0f06a58cd7f90c7db",
+    "url": "/eXchange/static/media/addIcon.bf58f165.svg"
+  },
+  {
+    "revision": "b89b3056802d0edeef2ba401bc25140f",
+    "url": "/eXchange/static/media/alert-close.b89b3056.svg"
+  },
+  {
+    "revision": "7882b05154c535a2190b29aa62f13825",
+    "url": "/eXchange/static/media/alert-open.7882b051.svg"
+  },
+  {
+    "revision": "2174843005dd5518ca4071a88dcf0940",
+    "url": "/eXchange/static/media/bgi.21748430.jpg"
+  },
+  {
+    "revision": "0aa8793c761ad532493786e5061c32e5",
+    "url": "/eXchange/static/media/directoryBgi.0aa8793c.jpg"
+  },
+  {
+    "revision": "e8086b8228ee82fdefb5d7936527e43e",
+    "url": "/eXchange/static/media/feed.e8086b82.svg"
+  },
+  {
+    "revision": "f9ca1302b9458ec0709278f8e04f248f",
+    "url": "/eXchange/static/media/logo.f9ca1302.svg"
+  },
+  {
+    "revision": "76118bea7d7764ef1effe9e6edb6ee65",
+    "url": "/eXchange/static/media/logout.76118bea.svg"
+  },
+  {
+    "revision": "73d96c4934ee2b4870daaf3b7580093b",
+    "url": "/eXchange/static/media/profile.73d96c49.svg"
+  },
+  {
+    "revision": "a88f77554bd97c8632e77132d37d6e8e",
+    "url": "/eXchange/static/media/search.a88f7755.svg"
+  },
+  {
+    "revision": "83d38c97f53f25089780bc641ccd226e",
+    "url": "/eXchange/static/media/settings.83d38c97.svg"
+  }
+]);
